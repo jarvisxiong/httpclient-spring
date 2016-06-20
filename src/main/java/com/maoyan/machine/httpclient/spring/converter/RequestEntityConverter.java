374f0513d71207b11ae86c74c16cebe5a0db9e65
xujia06 <xujia06@meituan.com>
2016-06-20 20:54:35 +0800

    asdf

--- src/main/java/com/maoyan/machine/httpclient/spring/converter/RequestEntityConverter.java
+++ src/main/java/com/maoyan/machine/httpclient/spring/converter/RequestEntityConverter.java
@@ -10,7 +10,7 @@ import java.nio.charset.Charset;
 import org.apache.http.HttpEntity;
 
 /**
- * TODO 在这里编写类的功能描述
+ * 用于java对象转换成http请求体
  * 
  * @author xujia06
  * @created 2016年6月3日
@@ -18,7 +18,16 @@ import org.apache.http.HttpEntity;
  * @version 1.0
  */
 public interface RequestEntityConverter {
+    /**
+     * @param object 请求对应的java对象
+     * @param charset 请求体字符集
+     * @return HttpEntity
+     * @throws Exception
+     */
     HttpEntity getEntity(Object object, Charset charset) throws Exception;
 
+    /**
+     * 返回该Converter处理的MimeType
+     */
     String getMimeType();
 }
--- src/main/java/com/maoyan/machine/httpclient/spring/converter/ResponseEntityConverter.java
+++ src/main/java/com/maoyan/machine/httpclient/spring/converter/ResponseEntityConverter.java
@@ -11,13 +11,24 @@ import java.nio.charset.Charset;
 import org.apache.http.HttpEntity;
 
 /**
+ * 用于将响应体反序列化为java对象
+ * 
  * @author xujia06
  * @created 2016年6月3日
  * 
  * @version 1.0
  */
 public interface ResponseEntityConverter {
+    /**
+     * @param httpEntity 响应实体
+     * @param type 被反序列化对象的类型
+     * @param charset 响应头字符集
+     * @return
+     * @throws Exception
+     */
     Object convert(HttpEntity httpEntity, Type type, Charset charset) throws Exception;
-
+    /**
+     * 返回该Converter处理的MimeType
+     */
     String getMimeType();
 }
